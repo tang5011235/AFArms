e8f9029c6b1d8170cd76efd198e1950bb729582b
394380623@qq.com <394380623@qq.com>
2018-04-24 10:59:36 +0800

    app support lib

--- app/src/main/java/com/af/demo/MainActivity.java
+++ app/src/main/java/com/af/demo/MainActivity.java
@@ -3,8 +3,6 @@ package com.af.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.targzon.afarms.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
